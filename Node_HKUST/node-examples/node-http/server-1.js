8eea888daac85f9f1f55ba7b3fc60a5a3e72fe17
Subramanyammolleti <Subbumolleti002@gmail.com>
2017-05-29 19:23:17 +0530

    Node and HTTP

---
+++ Node_HKUST/node-examples/node-http/server-1.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var server = http.createServer(function(req, res){
+
+	console.log(req.headers);
+
+	res.writeHead(200, {'Content-type': 'text/html'});
+	res.end('<h1>Hello World</h1>');
+
+});
+
+
+server.listen(port, hostname, function(){
+	console.log(`Server has just started at http://${hostname}:${port}/`);
+});
